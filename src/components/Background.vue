b4c2c545ab38fa81787f5b21e76162e33e003b53
Arya00ffff <abd.m.abd.1356@gmail.com>
2025-06-02 04:46:04 +0330

    Let's not talk about this.

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,21 +1,24 @@
 <script setup>
-import { ref, onMounted } from 'vue'
 
-const container = ref(null)
-
-onMounted(() => {
-  const tile = document.createElement('div')
-  tile.className = 'tile'
-  for (let i = 0; i < 1599; i++) {
-    container.value.appendChild(tile.cloneNode())
-  }
-})
 </script>
+
 <template>
-    <div id="container" ref="container"></div>
+  <div id="container"></div>
 </template>
+
 <style scoped>
-#container{
+#container {
+  width: 120rem;
+  aspect-ratio: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 0;
   z-index: -1;
+
+
+  background:
+    repeating-linear-gradient(to right, #292737 0 1px, transparent 1px 3rem),
+    repeating-linear-gradient(to bottom, #292737 0 1px, transparent 1px 3rem);
 }
 </style>
